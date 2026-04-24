b1407d03e46b54646f21450298aab118906b409d
govindjoshi9 <joshigovind740@gmail.com>
2026-04-24 17:19:59 +0530

    index add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require('path');
 
 dotenv.config();
 
@@ -22,9 +23,9 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/courses', require('./routes/courseRoutes'));
 app.use('/api/enrollments', require('./routes/enrollmentRoutes'));
 
-// app.get("/", (req, res) => {
-//     res.send("API is running...");
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
